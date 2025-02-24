4b7163dfba29a32ceba8876a5e1799f120e2acda
swappy-2003 <dubeyswapnil.123@gmail.com>
2025-02-24 22:10:16 +0530

    fix: correct import path for createUser action in webhook route

--- src/app/api/webhooks/clerk/route.js
+++ src/app/api/webhooks/clerk/route.js
@@ -1,7 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
-import { createUser } from "@/actions/user.action";
+import { createUser } from "../../../../../actions/user.actions";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
