faca5b0e854ad23f71c7fdc40d344c0540ba2cd0
swappy-2003 <dubeyswapnil.123@gmail.com>
2025-03-07 16:24:49 +0530

    feat: integrate Prisma for database management; add user model and webhook handling

--- src/components/DemoSection.js
+++ src/components/DemoSection.js
@@ -1,8 +1,13 @@
 import SparklesIcon from "@/components/SparklesIcon";
+import prisma from "@/lib/db";
 
-export default function DemoSection() {
+export default async function DemoSection() {
+  const users = await prisma.user.findMany();
   return (
     <section className="flex justify-around mt-8 sm:mt-12 items-center">
+      <div className="  ">
+        {JSON.stringify(users)}
+      </div>
       <div className="hidden sm:block bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
         <video src="https://dawid-epic-captions.s3.us-east-1.amazonaws.com/without-captions.mp4" preload muted autoPlay loop></video>
       </div>
